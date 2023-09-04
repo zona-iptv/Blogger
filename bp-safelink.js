24586e29750db4109413a4e449d0ab7709cd4c9e
Γιώργος <85579397+zona-iptv@users.noreply.github.com>
2023-09-04 11:12:58 +0300

    Delete bp-safelink.js

--- bp-safelink.js
+++
@@ -1,8 +0,0 @@
-var downloadUrl=document["querySelectorAll"](".safe");
-for(let i=0;
-i< downloadUrl["length"];
-	i++){
-	const dlink=downloadUrl[i]["href"];
-	var uncodeurl=dlink["href"]= btoa(dlink);
-	downloadUrl[i]["href"]= safepageurl+ "?o=""+ uncodeurl
-}
